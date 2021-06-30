00df175ec3d0ea7e75f175d6f27d706be0fc5bd2
gustavo <granzb11@hotmail.com>
2021-06-30 17:47:10 -0400

    added ItemList

--- simple-flask-restful-api/code/app.py
+++ simple-flask-restful-api/code/app.py
@@ -4,10 +4,24 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404 # 404 is for not found
 
-api.add_resource(Student, '/student/<string:name>') # http://127.0.0.1:5000/student/Rolf
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201 # 201 is used for "created", 202 is when you are delaying the creation because maybe it takes 10 mins to create your object. At that point the client gets a successful return code and can move along
 
-app.run(port=5000)
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/<>
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
