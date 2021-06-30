ecdb1eb4705bb49de6d33d9ff9c37ab3b25e21a1
gustavo <granzb11@hotmail.com>
2021-06-30 17:56:38 -0400

    added ItemList

--- simple-flask-restful-api/code/app.py
+++ simple-flask-restful-api/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -7,18 +7,43 @@ api = Api(app)
 items = []
 class Item(Resource):
     def get(self, name):
+        """Return back item based on name
+
+        Args:
+            name (String): Name of item to search
+
+        Returns:
+            Dictionary: Item found
+        """
         for item in items:
             if item['name'] == name:
                 return item
         return {'item': None}, 404 # 404 is for not found
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        """Creates an item
+
+        Args:
+            name (string): Name of item
+
+        Returns:
+            Dictionary: Newly created item.
+        """
+        # if the content-type is not set or the body of the request is not in JSON format, we will get a failure in line 16. To avoid these failures we can use the following 2 solutions.
+        # force=True - this means that you do not need the "content-type" to be set, it will look in the content and format it even if the content-type is not set. This is not recommended as then you will never look at the header and ALWAYS perform the processing to format the body into JSON
+        # silent=True - this does not give an error and returns None
+        data = request.get_json(force=True)
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201 # 201 is used for "created", 202 is when you are delaying the creation because maybe it takes 10 mins to create your object. At that point the client gets a successful return code and can move along
 
 class ItemList(Resource):
     def get(self):
+        """Returns back a list of items
+
+        Returns:
+            Dictionary: List of items
+        """
         return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/<>
