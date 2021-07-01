1d2b958570dfb63e08a0f4bd6e3fc3cad0d98194
gustavo <granzb11@hotmail.com>
2021-07-01 11:43:20 -0400

    added secure string compare

---
+++ simple-flask-restful-api/code/user.py
@@ -0,0 +1,7 @@
+class User:
+    """User Class for user data
+    """
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
