8332562fd3700475ffd75c448aa73140501caa8e
gustavo <granzb11@hotmail.com>
2021-07-11 13:04:32 -0400

    updates

--- section-6-sql-alchemy/code/models/user.py
+++ section-6-sql-alchemy/code/models/user.py
@@ -23,7 +23,7 @@ class UserModel(db.Model):
 
     def delete_from_db(self):
         db.session.delete(self)
-        db.session.commit()        
+        db.session.commit()
 
     @classmethod
     def find_by_username(cls, username):
