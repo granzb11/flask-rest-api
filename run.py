aa1e20a311507493acd15a16f099ec3adb02712d
gustavo <granzb11@hotmail.com>
2021-07-13 09:06:58 -0400

    updates

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from db import db
 from app import app
 
-db_init_app(app)
+db.init_app(app)
 
 @app.before_first_request
 # this will run before any request is served and it's going to create the tables for us if they don't already exist
